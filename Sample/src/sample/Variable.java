b799b02de4b49425298ca981ab04ed35813c3bc6
dir-FY24ayakakubota <ayaka.kubota.z489@dirbato.co.jp>
2024-04-08 10:49:47 +0900

    コミット

---
+++ Sample/src/sample/Variable.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        var number1  = 1; 
+        System.out.println(number1);
+        
+        String strl; 
+        strl  =  "abc"; 
+        System.out.println(strl);
+                
+    }
+
+}
